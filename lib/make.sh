a706a95c5065d025c5a6f6b24e4d176d7a65cb97
ShengYg <shengy0726@gmail.com>
2017-10-17 15:47:26 +0800

    make

--- lib/make.sh
+++ lib/make.sh
@@ -14,3 +14,15 @@ nvcc -c -o roi_pooling.cu.o roi_pooling_kernel.cu \
 #	roi_pooling_op.cu.o -I $TF_INC -fPIC -lcudart -L $CUDA_PATH/lib64
 cd ../../
 python build.py
+
+# rm nms/cpu_nms.c
+# rm nms/cpu_nms.so
+# rm nms/gpu_nms.cpp
+# rm nms/gpu_nms.so
+# rm roi_pooling/_ext/roi_pooling/_roi_pooling.so
+# rm roi_pooling/src/cuda/roi_pooling.cu.o
+# rm utils/bbox.c
+# rm utils/cython_bbox.so
+# rm utils/cython_nms.so
+# rm utils/nms.c
+
